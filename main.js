9f40cd0e8c7d2f7335e4491c2feb42f4c7b3ded8
peterzhao123 <peter_zhao92@hotmail.com>
2017-03-28 13:44:55 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
   var $curr_record;
   
   $curr_record = $('#record-item-template').clone();
+  
+  $curr_record.find("#entry-title").text("change it boi");
   $curr_record.css('display','block');
   $('#details-list').append($curr_record);
 });
