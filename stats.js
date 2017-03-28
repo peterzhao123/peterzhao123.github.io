712317ac908be69b1e93c620f92632024234ddde
peterzhao123 <peter_zhao92@hotmail.com>
2017-03-28 19:48:36 -0400

    Update stats.js

--- stats.js
+++ stats.js
@@ -1,5 +1,19 @@
 var records = [
   // Date, Map, Peter Time, Zheng Time
   ["March 13", "No Skidding", 20.341, 21.755],
-  ["March 14", "Spartan Racing", 33.798, 32.468]
-  ];
+  ["March 14", "Spartan Racing", 33.798, 32.468],
+  ["March 15", "The Dip", 19.650, 21.553],
+  ["March 16", "Fury Road", 27.544, 30.511],
+  ["March 17", "Metal Gear", 50.461, 29.869],
+  ["March 18", "Lost in Transmission", 23.124, 22.921],
+  ["Weekly", "Bottom Gear", 21.438, 23.659],
+  ["March 19", "Belter Road", 45.238, 44.256],
+  ["March 20", "Cliffside Way", 35.131, 35.202],
+  ["March 21", "Landing Drive", 18.464, 21.159],
+  ["March 22", "Bridge And Stones", 21.525, 22.700],
+  ["March 23", "Bill's Landing", 30.678, 28.240],
+  ["March 24", "N/A", 0, 0],
+  ["March 25", "Ballmer's Peak", 32.731, 30.959],
+  ["March 26", "Tricky Drive", 0.000, 28.557],
+  ["Weekly", "Big Air", 18.550, 19.425]
+];
