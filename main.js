39b736fb823df70a4dfa27a4649f6fe01ec34597
peterzhao123 <peter_zhao92@hotmail.com>
2017-03-28 19:17:27 -0400

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,10 @@ $(document).ready(function(){
   $curr_record = $('#record-item-template').clone();
   
   $curr_record.attr("id","gibberish");
-  $curr_record.find('.entry-title').text("change it boi");
+  $curr_record.find('.entry-title').text("the winner");
+  $curr_record.find('.race-date').text("the date");
+  $curr_record.find('.race-name').text("the map");
+  $curr_record.find('#time-block').text("the time");
   $curr_record.css('display','block');
   $('#details-list').append($curr_record);
 });
