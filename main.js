f250b0e06d51eca2df91f3e521726f718895abd0
peterzhao123 <peter_zhao92@hotmail.com>
2017-03-28 19:22:44 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 $(document).ready(function(){
   var $curr_record;
-  
-  $curr_record = $('#record-item-template').clone();
-  
-  $curr_record.attr("id","gibberish");
-  $curr_record.find('.entry-title').text("the winner");
-  $curr_record.find('.race-date').text("the date");
-  $curr_record.find('.race-name').text("the map");
-  $curr_record.find('#time-block').text("the time");
-  $curr_record.css('display','block');
-  $('#details-list').append($curr_record);
+  var i; 
+  for (i = 0; i < records.length; i++) { 
+    $curr_record = $('#record-item-template').clone();
+
+    $curr_record.attr("id",i);
+    $curr_record.find('.entry-title').text("need to calculate");
+    $curr_record.find('.race-date').text(records[i][0]);
+    $curr_record.find('.race-name').text(records[i][1]);
+    $curr_record.find('#time-block').text("Peter time:",records[i][2],"Zheng time:",records[i][3]);
+    $curr_record.css('display','block');
+    $('#details-list').append($curr_record);
+  }
 });
