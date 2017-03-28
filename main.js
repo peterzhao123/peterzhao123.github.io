979eeea9bc5b0671e2bf9d96189264ff146f7100
peterzhao123 <peter_zhao92@hotmail.com>
2017-03-28 19:24:09 -0400

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
     $curr_record.find('.entry-title').text("need to calculate");
     $curr_record.find('.race-date').text(records[i][0]);
     $curr_record.find('.race-name').text(records[i][1]);
-    $curr_record.find('#time-block').text("Peter time:",records[i][2],"Zheng time:",records[i][3]);
+    $curr_record.find('#time-block').text("Peter time: "+records[i][2]+"Zheng time: "+records[i][3]);
     $curr_record.css('display','block');
     $('#details-list').append($curr_record);
   }
