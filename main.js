115a700b802aeab9dba897047abc5c42e8e22188
peterzhao123 <peter_zhao92@hotmail.com>
2017-03-28 21:23:47 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
   var $curr_record;
   var i;
   var winner;
+  var winnerbol;
   var cumpet = 0;
   var cumzhe = 0;
   var currpet = 0;
@@ -16,28 +17,33 @@ $(document).ready(function(){
       winner = "No Winner";
       currpet = 0;
       currzhe = 0;
+      winnerbol = 0;
     }
     // Peter did not finish
     else if (records[i][2] == 0){
       currpet = 0;
       currzhe = 2;
+      winnerbol = 2;
     }
     // Zheng did not finish
     else if (records[i][2] == 0){
       currpet = 2;
       currzhe = 0;
+      winnerbol = 1;
     }
     // Peter lower time than Zheng
     else if (records[i][2] < records[i][3]){
       winner = "Winner: Peter";
       currpet = 2;
       currzhe = 1;
+      winnerbol = 1;
     }
     // Zheng lower time than Peter
     else {
       winner = "Winner: Zheng";
       currpet = 1;
       currzhe = 2;
+      winnerbol = 2;
     }
     // Weekly multiplier
     if(records[i][0] == "Weekly"){
@@ -50,6 +56,12 @@ $(document).ready(function(){
     $curr_record.find('.race-name').text(records[i][1]);
     $curr_record.find('#time-block').html("Peter time: "+records[i][2]+" Score: "+currpet+"<br />Zheng time: "+records[i][3]+" Score: "+currzhe);
     $curr_record.css('display','block');
+    if(winnerbol == 1){
+      $curr_record.css('background','#003333');
+    }
+    else if(winnerbol ==2){
+      $curr_record.css('background','#003399');
+    }
     $('#details-list').append($curr_record);
     cumpet = cumpet + currpet;
     cumzhe = cumzhe + currzhe;
