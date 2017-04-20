34e32bfe2247fd5cc03c38ae403a582637439494
peterzhao123 <peter_zhao92@hotmail.com>
2017-04-19 22:00:44 -0400

    Update basketstar.js

--- basketstar.js
+++ basketstar.js
@@ -1,13 +1,24 @@
 $(document).ready(function(){
-  var currow = "<tr>\
-  <td>A</td>\
-  <td>A</td>\
-  <td>A</td>\
-  <td>A</td>\
-  <td>A</td>\
-  <td>A</td>\
-  <td>A</td>\
-  </tr>";
+  var pearn = 0;
+  var zearn = 0;
+  var pwinper = 0;
+  var zwinper = 0;
+  var zwin = 0;
+  var zloss = 0;
   
-  $('#content-table').append(currow);
+  for (i = 0; i < bsrecords.length; i++){
+    pwinper = Math.floor((bsrecords[i][3] / bsrecords[i][4]) * 100);
+    zwinper = Math.floor((bsrecords[i][5] / bsrecords[i][6]) * 100);
+    var currow = "<tr>\
+      <td>"+bsrecords[i][0]+"</td>\
+      <td>"+bsrecords[i][1]+"</td>\
+      <td>"+bsrecords[i][2]+"</td>\
+      <td>"+bsrecords[i][3]+":"+bsrecords[i][4]+"</td>\
+      <td>"+bsrecords[i][5]+":"+bsrecords[i][6]+"</td>\
+      <td>"+pwinper+"%</td>\
+      <td>"+zwinper+"%</td>\
+      </tr>";
+
+    $('#content-table').append(currow);
+  };
 });
