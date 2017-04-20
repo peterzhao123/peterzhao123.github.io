119b9a9a484a3c26c1645c46fd995dff75907283
peterzhao123 <peter_zhao92@hotmail.com>
2017-04-19 21:43:13 -0400

    Update basketstar.js

--- basketstar.js
+++ basketstar.js
@@ -1,2 +1,13 @@
 $(document).ready(function(){
+  var currow = "<tr>\
+  <td>A</td>\
+  <td>A</td>\
+  <td>A</td>\
+  <td>A</td>\
+  <td>A</td>\
+  <td>A</td>\
+  <td>A</td>\
+  </tr>";
+  
+  $('content-table').append(currow);
 });
